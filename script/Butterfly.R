50e1eb05c7161aaafc2b991a3a5d6d1743cc6dc2
ubd2751 <iwachi.yuki@gmail.com>
2023-10-27 20:07:28 +0900

    231027

--- script/Butterfly.R
+++ script/Butterfly.R
@@ -8,16 +8,44 @@ pacman::p_load(
   )
 
 
+# For read_csv
+options(
+  readr.num_columns = 0L,
+  readr.show_col_types = FALSE,
+  readr.show_progress = FALSE
+)
+
+
 # load a data frame
 butterfly <- read_csv("data/butterfly/df_butterfly.csv")
-butterfly_trait <- read_csv("data/butterfly/butterfly_trait.csv")
+
 butterfly_sci <- read_csv("data/butterfly/butterfly_sciname.csv")
+
+
+
+# organization of the trait data
+butterfly_trait <- read_csv("data/butterfly/butterfly_trait.csv")
+
+#df_trait_butterfly <-
+  butterfly_trait %>% 
+  dplyr::select(
+    -Family, -Genus, -verbatimSpecies,
+    -WS_L_Fem:-WS_U_Mal, -FW_L_Fem:-FW_U_Mal, -Jan:-Dec,
+    -NumberOfHostplantFamilies:-EqualHostplantFamily, -DateCreated
+    ) %>% 
+  dplyr::filter(!is.na(OvipositionStyle))
+  dplyr::mutate(
+    WS = mean(c(WS_L, WS_U)),
+    FW = mean(c(FW_L, FW_U))
+    ) 
+  
 
 
 
 
 # data frmae for butterfly
-df_butter <- butterfly %>% 
+df_butterfly <- 
+  butterfly %>% 
   dplyr::mutate(time = if_else(str_detect(site, "過去"), "past", "now")) %>% 
   dplyr::select(site, time, everything()) %>% 
   pivot_longer(cols = -c(site, time), names_to = "species") %>% 
@@ -36,10 +64,22 @@ df_butter <- butterfly %>%
     ) %>%
   dplyr::filter(species != "オオスカシバ") %>% 
   left_join(butterfly_sci, by = "species") %>% 
-  left_join(butterfly_trait, by = c("sci_name" = "Species"))
+  dplyr::mutate(
+    sci_name = recode(
+      sci_name,
+      "Everes argiades" = "Cupido argiades",
+      "Cynthia cardui" = "Vanessa cardui",
+      "Narathura bazalus" = "Narathura bazalus",
+      "Damora sagana" = "Argynnis sagana"
+      )
+    ) %>%
+  left_join(butterfly_trait, by = c("sci_name" = "Species")) %>% 
+  dplyr::select(-Jan:-Dec, -DateCreated)
   
-  
-  
+
+
+df_butterfly %>% 
+  summary()
   
 
 
